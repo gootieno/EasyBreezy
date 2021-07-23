daece7e14d7324858675cc69f4a944bdca8e1ea7
geoffrey otieno <gotieno@geoffreys-MBP.attlocal.net>
2021-07-23 14:41:42 -0700

    imported material ui and set up routes

--- frontend/src/components/Destination/index.js
+++ frontend/src/components/Destination/index.js
@@ -3,12 +3,16 @@ import Event from "../../components/Event";
 
 function Destination() {
   const [events, setEvents] = useState([1, 2, 3, 4]);
+
   return (
-    <div>
+    <>
       {events.map((event) => (
-        <Event event={event} />
+        <>
+          <Event event={event} />
+          <br />
+        </>
       ))}
-    </div>
+    </>
   );
 }
 
--- frontend/src/components/Event/index.js
+++ frontend/src/components/Event/index.js
@@ -1,9 +1,52 @@
 import React from "react";
+import { useHistory } from "react-router-dom";
+import { makeStyles } from "@material-ui/core/styles";
+import Card from "@material-ui/core/Card";
+import CardActions from "@material-ui/core/CardActions";
+import CardContent from "@material-ui/core/CardContent";
+import Button from "@material-ui/core/Button";
+import Typography from "@material-ui/core/Typography";
 
+const useStyles = makeStyles({
+  root: {
+    minWidth: 275,
+  },
+  title: {
+    fontSize: 14,
+  },
+  pos: {
+    marginBottom: 12,
+  },
+  cardContainer: {},
+});
 function Event({ event }) {
+  const classes = useStyles();
+
+  const history = useHistory();
+
+  const handleCardContainer = (e) => {
+    const id = String(e.target.id);
+    history.push(`/events/${id}`);
+  };
+
   return (
-    <div>
-      <h1>This is event number: {event}</h1>
+    <div
+      onClick={handleCardContainer}
+      className={classes.cardContainer}
+      id={event}
+    >
+      <Card className={classes.root}>
+        <CardContent>
+          <Typography
+            className={classes.title}
+            color="textSecondary"
+            gutterBottom
+          >
+            Event Number {event}
+          </Typography>
+          <Typography>Event Date Here</Typography>
+        </CardContent>
+      </Card>
     </div>
   );
 }
