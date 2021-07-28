9184d569acc112c07e41c7a64d2ecaafd950a8cd
geoffrey otieno <gotieno@geoffreys-MBP.attlocal.net>
2021-07-28 15:46:39 -0700

    modified landing page

--- frontend/src/components/LandingPage/index.js
+++ frontend/src/components/LandingPage/index.js
@@ -1,11 +1,18 @@
 import { Typography } from "@material-ui/core";
+import { useHistory } from "react-router-dom";
 import React from "react";
 import "./index.css";
 
 function LandingPage() {
+  const history = useHistory();
+  const handleClick = (e) => {
+    history.push("/destinations");
+  };
   return (
     <div className="landing-page-container">
-      <Typography id="landing-page-title">TravelMe</Typography>
+      <Typography onClick={handleClick} id="landing-page-title">
+        TravelPlans
+      </Typography>
     </div>
   );
 }
